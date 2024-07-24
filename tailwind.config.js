ffafafaba9ee073c532d2eae09e48de2c0e4def3
IsmoilovO <omismoilov@gmail.com>
2024-07-24 11:38:53 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+    },
+    container: {
+      padding: "24px",
+      center: true,
+      screens: {
+        lg: "1320px",
+      },
+    },
+    },
+  }
